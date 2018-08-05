31e992435d7c8f09b61efe0a6a9efb630b0ea5d2
Lubomir <lubomirkavetskiy@gmail.com>
2018-08-05 22:54:25 +0300

    use object destructuring and change title of currentElem of forEach()

--- src/ProductService.js
+++ src/ProductService.js
@@ -3,13 +3,13 @@ import { get, post } from './HttpService';
 const API = `http://159.89.106.160/products`;
 const endPoint = `http://159.89.106.160/products/sendemail`;
 
-export const getAllProducts = () => get(API).then(data => data.data);
+export const getAllProducts = () => get(API).then(({ data }) => data);
 
 export const getOneProduct = id => 
   get(`${API}/${id}`).then(({ data, product }) => {
-    data.forEach(arr => arr.pricingDataByWeek.forEach(arr => {
+    data.forEach(obj => obj.pricingDataByWeek.forEach(obj => {
       const stringToArr = arr.week.split(`/`);
-      arr.week = stringToArr.splice(0, 2).reverse().concat(stringToArr.pop()).join('/');
+      obj.week = stringToArr.splice(0, 2).reverse().concat(stringToArr.pop()).join('/');
     }));
     return ({ 
       name: product.name,
